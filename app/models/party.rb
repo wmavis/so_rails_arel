7cf2cdd14862e2d21e1cba3f7010299453bf7fde
Will Mavis <willard.mavis@gmail.com>
2018-06-11 11:52:04 -0500

    Add Party code

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,2 +1,11 @@
 class Party < ApplicationRecord
+#  has_one :name,
+#    -> { valid },
+#    class_name: 'PartyName',
+#    foreign_key: :partyId,
+#
+#  has_many :expired_names,
+#    -> { invalid },
+#    class_name: 'PartyName',
+#    foreign_key: :partyId,
 end
